4866c1e4df4942925a14b2c29c9c92821bb08483
Mike Pennington <mpenning@users.noreply.github.com>
2021-11-29 08:12:10 -0600

    Update run_ansible.sh

--- run_ansible.sh
+++ run_ansible.sh
@@ -6,4 +6,4 @@ DEFAULT=$USER
 read -p "Ansible SSH username (default: $DEFAULT): " ansible_username
 ansible_username="${ansible_username:-${DEFAULT}}"
 rm ./ansible.log
-ansible-playbook -vvv -u $ansible_username --ask-pass --flush-cache -i inventory.ini cisco_play.yml
+ansible-playbook -u $ansible_username --ask-pass --flush-cache -i inventory.ini cisco_play.yml
